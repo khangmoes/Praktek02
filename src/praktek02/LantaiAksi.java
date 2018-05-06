4b9e3a98aeb2659a1f5f9ddc8f1aff77be74f540
Musthofa <musthofa.cikitew@gmail.com>
2018-05-06 20:21:10 +0800

    Mengisi nilai attribute

--- src/praktek02/LantaiAksi.java
+++ src/praktek02/LantaiAksi.java
@@ -3,6 +3,7 @@ package praktek02;
 public class LantaiAksi {
    public static void main(String[] args) {
        Lantai L1 = new Lantai();
-       
+       L1.panjang = 5;
+       L1.lebar = 4;
    } 
 }
